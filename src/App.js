8ffd8281f0e199e7b2e659a309888335fab183c6
Abhinay <thadakaabhinay@gmail.com>
2024-08-14 21:40:03 +0530

    added test login credits in authlogin

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Home,SearchResults,SingleHotel} from "./pages"
+import {Home,SearchResults,SingleHotel,Wishlist} from "./pages"
 import './App.css';
 import { Route,Routes } from "react-router-dom";
 
@@ -9,7 +9,7 @@ function App() {
         <Route path="/" element={  <Home />}/>
         <Route path="/hotels/:name/:address/:id/reserve" element={<SingleHotel />}/>
         <Route path="/hotels/:address" element={<SearchResults />} />
-
+        <Route path="/wishlist" element={<Wishlist />} />
     </Routes>
   
     
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,3 +1,4 @@
 export {dateReducer} from "./date-reducer";
 export {filterReducer} from "./filter-reducer";
 export {authReducer} from "./auth-reducers";
+export {wishlistReducer} from "./wishlist-reducer";
